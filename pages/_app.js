5db2d2158d8a2b53533b9d2eafc0e9d5aa53a293
Justus-Becker-Rabbenstein <43116175+Justus-Becker-Rabbenstein@users.noreply.github.com>
2022-11-29 13:27:55 +0100

    fixed prettier error

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import {useState} from "react";
 
 function MyApp({Component, pageProps}) {
   const [loginName, setLoginName] = useState("Enter user name ... ");
-  const onSubmitLogin = (loginNameParam) => {
+  const onSubmitLogin = loginNameParam => {
     setLoginName(loginNameParam);
   };
 
