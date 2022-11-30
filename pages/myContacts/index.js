c8758bf4c7a69a191ef45dea78be25461fe7146a
Justus-Becker-Rabbenstein <43116175+Justus-Becker-Rabbenstein@users.noreply.github.com>
2022-11-30 12:00:27 +0100

    implemented the mycontacts page with array of users in pages/_db/dbArray.js to be displayed in pages/myContacts/index.js which is opend after clicking submit on the login page

--- pages/myContacts/index.js
+++ pages/myContacts/index.js
@@ -3,22 +3,46 @@ import styled from "styled-components";
 const myContacts = ({userArray}) => {
   return (
     <article>
-      <header><h2>My Contacts</h2></header>
-     {userArray.map((singleUser)=>{
-       return (
-        <ContainerSection>
-          <ContainerImg src={singleUser.image} alt={singleUser.name} height="50vh" width="50vw" />
-          <ContainerHeader>
-            <h3>{singleUser.name}</h3>
-          </ContainerHeader>
-        <ContainerName><img src="icon_name.svg" alt="Icon of name" /><p>{singleUser.name}</p></ContainerName>
-        <ContainerAddress><img src="icon_address.svg" alt="Icon of address" /><p>{singleUser.address}</p></ContainerAddress>
-        <ContainerEmail><img src="icon_email.svg" alt="Icon of email" /><p>{singleUser.email}</p></ContainerEmail>
-        <ContainerPhone><img src="icon_phone.svg" alt="Icon of phone" /><p>{singleUser.phone}</p></ContainerPhone>
-        <ContainerWebsite><img src="icon_website.svg" alt="Icon of website" /><p>{singleUser.website}</p></ContainerWebsite>
-        </ContainerSection>
-       )
-     })}
+      <header>
+        <h2>My Contacts</h2>
+      </header>
+      {userArray.map(singleUser => {
+        return (
+          <>
+            <ContainerSection>
+              <ContainerProfileImage
+                src={singleUser.image}
+                alt={singleUser.name}
+                height="50vh"
+                width="50vw"
+              />
+              <ContainerHeader>
+                <h3>{singleUser.name}</h3>
+              </ContainerHeader>
+              <Container>
+                <ContainerIcon src="icon_name.svg" alt="Icon of name" />
+                <ContainerParagraph>{singleUser.name}</ContainerParagraph>
+              </Container>
+              <Container>
+                <ContainerIcon src="icon_address.svg" alt="Icon of address" />
+                <ContainerParagraph>{singleUser.address}</ContainerParagraph>
+              </Container>
+              <Container>
+                <ContainerIcon src="icon_email.svg" alt="Icon of email" />
+                <ContainerParagraph>{singleUser.email}</ContainerParagraph>
+              </Container>
+              <Container>
+                <ContainerIcon src="icon_phone.svg" alt="Icon of phone" />
+                <ContainerParagraph>{singleUser.phone}</ContainerParagraph>
+              </Container>
+              <Container>
+                <ContainerIcon src="icon_website.svg" alt="Icon of website" />
+                <ContainerParagraph>{singleUser.website}</ContainerParagraph>
+              </Container>
+            </ContainerSection>
+          </>
+        );
+      })}
     </article>
   );
 };
@@ -26,48 +50,43 @@ const myContacts = ({userArray}) => {
 export default myContacts;
 
 const ContainerSection = styled.section`
-display: grid;
-grid-template-columns: 20% 60% 20%;
-grid-template-rows: auto;
-gap: 3px;
-border-radius: 10px;
-width: 50vw;
-margin: 10px auto;
-background-image: url('isis-franca-4uZXCWaseNE-unsplash.jpg');
-background-size: cover;
-`
-const ContainerImg = styled.img`
-grid-column: 3;
-grid-row: 1;
-position: relative;
-right: -10px;
-border-radius: 100%;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  border-radius: 10px;
+  width: 80vw;
+  margin: 10px auto;
+  background-image: url("isis-franca-4uZXCWaseNE-unsplash.jpg");
+  background-size: cover;
+`;
+const ContainerProfileImage = styled.img`
+  position: absolute;
+  right: -10px;
+  border-radius: 100%;
+  height: 6em;
+  width: 6em;
+  border: 1px solid hotpink;
+`;
 const ContainerHeader = styled.header`
-grid-column: 2;
-grid-row: 1;
-border-radius: 10px;
-`
-const ContainerName = styled.div`
-grid-column: 2;
-grid-row: 2;
-width: 10%;
-
-`
-const ContainerAddress = styled.div`
-grid-column: 2;
-grid-row: 3;
-`
-const ContainerEmail = styled.div`
-grid-column: 2;
-grid-row: 4;
-`
-const ContainerPhone = styled.div`
-grid-column: 2;
-grid-row: 5;
-`
-const ContainerWebsite = styled.div`
-grid-column: 2;
-grid-row: 6;
-margin-bottom: 10px;
-`
+  border-radius: 10px;
+`;
+const Container = styled.div`
+  display: flex;
+  background-color: white;
+  border-radius: 10px;
+  width: 80%;
+  height: auto;
+  overflow: hidden;
+  margin: 3px;
+`;
+const ContainerParagraph = styled.p`
+  line-height: 0px;
+  padding-left: 3px;
+`;
+const ContainerIcon = styled.img`
+  width: 35px;
+  margin: 0px;
+  border-top-left-radius: 10px;
+  border-bottom-left-radius: 10px;
+`;
