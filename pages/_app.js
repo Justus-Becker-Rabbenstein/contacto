4f0ba23acc32196e3f1c3730998325612d18d821
Justus-Becker-Rabbenstein <43116175+Justus-Becker-Rabbenstein@users.noreply.github.com>
2022-12-09 14:31:09 +0100

    switched back variable values of testing

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ import Header from "../components/Header/Header";
 
 function MyApp({Component, pageProps}) {
   /* Start: Logic for Login credential saving */
-  const [loginName, setLoginName] = useState("Miss Piggy");
+  const [loginName, setLoginName] = useState("User name");
   const handleSubmitLogin = onSubmitLogin => {
     setLoginName(onSubmitLogin);
   };
