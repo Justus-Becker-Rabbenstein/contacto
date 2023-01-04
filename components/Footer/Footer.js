d4e8a003b21e658c56c0ae263df97dbca673771a
Justus-Becker-Rabbenstein <43116175+Justus-Becker-Rabbenstein@users.noreply.github.com>
2023-01-04 09:59:26 +0100

    deleted border radius of footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -25,7 +25,6 @@ const ContainerFooter = styled.footer`
   text-align: center;
 
   background: rgba(255, 255, 255, 0.1);
-  border-radius: 16px;
   box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
   backdrop-filter: blur(2.2px);
   -webkit-backdrop-filter: blur(2.2px);
